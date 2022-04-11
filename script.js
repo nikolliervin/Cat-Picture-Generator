99a9462fc8bf4a30f9a80445733a08cd7ee23f1c
Ervin <nikolliervin@gmail.com>
2022-04-12 00:22:59 +0200

    fetching pictures

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let generate_btn=document.querySelector(".generate-btn");
 generate_btn.addEventListener('click', fetchPics);
 function fetchPics(){
     fetch('https://api.thecatapi.com/v1/images/search').then(response=>response.json()).then((data)=>{
-        console.log(data);
+        let catImgUrl=data[0].url;
+        let catsImgEl=document.createElement("img");
+        catsImgEl.setAttribute('src',`${catImgUrl}`)
+        let catsImgDiv = document.querySelector(".catImg");
+        
+        catsImgDiv.appendChild(catsImgEl);
     })
+    .catch(err=>console.log(err))
 }
