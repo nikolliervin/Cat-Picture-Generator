93cd66f1edcb4fdb2d770458584f4c61449a4e43
Ervin <nikolliervin@gmail.com>
2022-04-12 00:37:43 +0200

    fixed the margin

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function fetchPics(){
     fetch('https://api.thecatapi.com/v1/images/search').then(response=>response.json()).then((data)=>{
         let catImgUrl=data[0].url;
         let catsImgEl=document.createElement("img");
+
         catsImgEl.setAttribute('src',`${catImgUrl}`)
+        catsImgEl.classList.add("showcase")
         let catsImgDiv = document.querySelector(".catImg");
         
         catsImgDiv.appendChild(catsImgEl);
